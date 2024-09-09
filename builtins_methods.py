bc7dbf02c5f8df3b58533a6e4ded256b7f0afe3e
jadhavpandu <2022bit054@sggs.ac.in>
2024-09-09 07:03:12 +0530

    all() builtin method

---
+++ builtins_methods.py
@@ -0,0 +1,18 @@
+# All builtins methods
+# 1. all()
+""" all() return True if it id iterable else it will return False 
+    if Iterable is empty it return True 
+"""
+print("hello")
+li = [1,2,3,4]
+print("to check list is iterable or not ::", all(li))
+
+st = {1,2,3,4}
+print("to check set is iterable or not::",all(st));
+
+dt = {"a":1, "b":2, "c":3}
+print("to check dict is iterable or not ::",all(dt))
+
+tp = (1,2,3)
+print("to check tuple is iterable or not ::",all(tp))
+    
