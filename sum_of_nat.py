94b9a012d21e183d2f89867e1313248190f933a6
jadhavpandu <2022bit054@sggs.ac.in>
2024-09-23 09:52:24 +0530

    approach 2 for sum of two natural numbers

--- sum_of_nat.py
+++ sum_of_nat.py
@@ -1,3 +1,10 @@
-def fun(n):
+# approach 1
+""" def fun(n):
     return n*(n+1)/2
-print("sum of natural numbers", fun(5))
+print("sum of natural numbers", fun(5))  """
+def fun1(n):
+    sum=0
+    for i in range(1,n+1):
+        sum = sum + i
+    return sum
+print("sum  is ", fun1(5))
